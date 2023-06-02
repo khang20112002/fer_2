c92e927e52afb16715fac4d3095d3eb007bdf418
khang20112002 <khangdcse161156@fpt.edu.vn>
2023-06-02 21:42:13 +0700

    Effect & Context Hook

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,75 @@
 import React from "react";
-import "./Navigation.css";
+import { useContext } from "react";
+import "./Navigation.css"
+import { ThemeContext } from './ThemeContext'
 
 export default function Navigation() {
-  return (
-    <nav>
-      <ul>
-        <div className="Header">
-          <div className="headerMenu">
-            <li>
-              <a className="active" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#news">News</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </div>
-        </div>
-      </ul>
-    </nav>
-  );
+    const { theme, toggle, dark } = useContext(ThemeContext)
+
+    const handleToggle = () => {
+        toggle();
+      };
+    return (
+        <>
+          <nav className="menu-list-container">
+            <ul>
+              <div
+                className="Header"
+                style={{
+                  backgroundColor: theme.backgroundColor,
+                  color: theme.color,
+                }}
+              >
+                <div className="headerMenu">
+                  <li>
+                    <a
+                      className={`${dark ? "color-menu-light" : "color-menu"}`}
+                      href="#home"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className={`${dark ? "color-menu-light" : "color-menu"}`}
+                      href="#news"
+                    >
+                      News
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className={`${dark ? "color-menu-light" : "color-menu"}`}
+                      href="#about"
+                    >
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className={`${dark ? "color-menu-light" : "color-menu"}`}
+                      href="#contact"
+                    >
+                      Contact
+                    </a>
+                  </li>
+                </div>
+              </div>
+            </ul>
+    
+            <div className="Toggle">
+                <li>
+                  <span
+                    className="switch-mode"
+                    onClick={handleToggle}
+                    data-testid="toggle-theme-btn"
+                  >
+                    Switch Nav to {!dark ? "Dark" : "Light"} mode
+                  </span>
+                </li>
+            </div>
+            
+          </nav>
+        </>
+      );
 }
