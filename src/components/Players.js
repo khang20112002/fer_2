7a872264b7fa1b67f4d51f984e65b14c9d4654db
khang20112002 <khangdcse161156@fpt.edu.vn>
2023-06-02 20:56:08 +0700

    State Hook

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
-import './Players.css';
 import { Players } from "../shared/ListOfPlayers";
+import { useState } from "react";
 
-export default class player extends Component {
-    render() {
-        return (
-            <div className='container'>
-                {Players.map((player)=>(
-                   <div className='column'>
-                   <div className='card'>
-                   <img src={player.img}/>
-                     <h3>{player.name}</h3>
-                     <p className='title'>{player.club}</p>
-                     <p><button>Detail</button></p>
-                   </div>
-                 </div>
-                ))}
-            </div>
-          )
-                      
-    }
+import "./Players.css";
+
+export default function Player() {
+  const [player, setPlayer] = useState([]);
+  const closePopUp = () => {
+    
+  }
+
+  return (
+    <div className="container">
+      {Players.map((player) => (
+        <div className="column">
+          <div className="card">
+            <img src={player.img} alt="" />
+            <h3>{player.name}</h3>
+            <p className="title"></p>
+            <p>
+              <button
+                onClick={() => {
+                  setPlayer(player);
+                }}
+              >
+                <a href="#popup1" id="openPopUp">
+                  Detail
+                </a>
+              </button>
+            </p>
+          </div>
+        </div>
+      ))}
+
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
