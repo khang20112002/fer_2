1ecf76430a89d270c385c0dd64e78777dc63d1d2
khang20112002 <khangdcse161156@fpt.edu.vn>
2023-06-02 20:47:56 +0700

    Functional Components

--- src/components/Players.js
+++ src/components/Players.js
@@ -17,6 +17,7 @@ export default class player extends Component {
                  </div>
                 ))}
             </div>
-          )              
+          )
+                      
     }
 }
