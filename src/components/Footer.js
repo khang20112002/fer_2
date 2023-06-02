7f3f0d6346d8cfb7e4466f50a1de9701b52ce917
khang20112002 <khangdcse161156@fpt.edu.vn>
2023-06-02 20:37:31 +0700

    Components Part 1

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                copyright ©️ 2020
+            </div>
+        )
+    }
+}
+
